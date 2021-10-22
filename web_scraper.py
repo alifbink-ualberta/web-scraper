04557ed332680c3be4f14015a472e5754a085646
Alif Bin Khorshed Ahmed <alifbink@ualberta.ca>
2021-10-21 19:06:36 -0600

    Program is now divided in functions.

--- web_scraper.py
+++ web_scraper.py
@@ -1,26 +1,45 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
 
-user_url = input("Enter url: ")
-
 #connect with webpage and get html data.
-url_req = urlopen(user_url)
-html_code = url_req.read()
-url_req.close()
+def get_webpage(user_url):
+    url_req = urlopen(user_url)
+    html_code = url_req.read()
+    url_req.close()
+    return html_code
 
 #parse html data.
-page_soup = soup(html_code, "html.parser")
+def parse_webpage(html_code):
+    page_soup = soup(html_code, "html.parser")
+    return page_soup
 
 #For pages in edition.cnn.com/travel:
 #title:
-title = page_soup.findAll("h1",{"class":"Article__title"})
+def print_title(page_soup):
+    title = page_soup.findAll("h1",{"class":"Article__title"})
+    print("\nTitle: " + title[0].text + "\n\n")
 
-print("\n" + title[0].text + "\n\n")
+#subtitle
+
+
+#latest date
+
 
 
 #article
-paragraphs = page_soup.findAll("div",{"class":"Paragraph__component"})
+def print_paragraphs(page_soup):
+    paragraphs = page_soup.findAll("div", class_ = "Paragraph__component")
 
-for c in paragraphs:
-    print(c.span.text)
-    print("\n")
+    print("Article: \n")
+    for c in paragraphs:
+        print(c.span.text + "\n")
+
+
+if __name__ == "__main__":
+    user_url = input("Enter url: ")
+    html_code = get_webpage(user_url)
+    page_soup = parse_webpage(html_code)
+    print_title(page_soup)
+    #print_subtitle(page_soup)
+    #print_latestdate(page_soup)
+    print_paragraphs(page_soup)
