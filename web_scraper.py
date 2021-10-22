878e096228697fa039ed1daf693d315f09e32233
Alif Bin Khorshed Ahmed <alifbink@ualberta.ca>
2021-10-21 18:02:03 -0600

    Code now connects with url and gets the content; start of parsing

--- web_scraper.py
+++ web_scraper.py
@@ -2,3 +2,11 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
 
 user_url = input("Enter url: ")
+
+#connect with webpage and get html data.
+url_req = urlopen(user_url)
+html_code = url_req.read()
+url_req.close()
+
+#parse html data.
+page_soup = soup(html_code, "html.parser")
